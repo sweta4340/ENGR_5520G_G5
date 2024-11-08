51647266cdac52a97558c93ffcb58394c208ffb4
sweta4340 <sweta.patel4340@gmail.com>
2024-11-08 00:23:35 -0500

    Adding test case for loan amount prediction

--- Parse_Input.py
+++ Parse_Input.py
@@ -5,87 +5,9 @@
 
 
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
-import json
 import joblib
 
-
-# In[2]:
-
-
-encoder = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/data_encoder.pkl')
-loan_scaler_x = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Loan_Amount_Prediction/loan_scaler_X.pkl')
-loan_scaler_y = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Loan_Amount_Prediction/loan_scaler_y.pkl')
-
-defaultprob_scaler_x = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Default_Probability_Prediction/defaultprob_scaler_X.pkl')
-defaultprob_scaler_y = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Default_Probability_Prediction/defaultprob_scaler_y.pkl')
-
-expreturn_scaler_x = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Expected_Return_Prediction/expreturn_scaler_X.pkl')
-expreturn_scaler_y = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Expected_Return_Prediction/expreturn_scaler_y.pkl')
-
-interest_scaler_x = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Interest_Rate_Prediction/interest_scaler_X.pkl')
-interest_scaler_y = joblib.load('C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Interest_Rate_Prediction/interest_scaler_y.pkl')
-
-
-# In[3]:
-
-
-# Loading the cleaned data after preprocessing.
-df = pd.read_csv("C:/Users/KRISHNA/Desktop/SHWETA/Fall 2024/ENGR_5520G_G5/Cleaned_Loan_approval_dataset.csv")
-
-
-# In[20]:
-
-
-df.head()
-
-
-# In[4]:
-
-
-record = df.loc[0, ['NewCreditCustomer', 'VerificationType', 'LanguageCode', 'Age',
-       'Gender', 'Country', 'AppliedAmount', 'UseOfLoan',
-       'Education', 'MaritalStatus', 'EmploymentStatus',
-       'EmploymentDurationCurrentEmployer', 'OccupationArea',
-       'HomeOwnershipType', 'DebtToIncome', 'FreeCash','Rating', 'Restructured', 'CreditScoreEsMicroL',
-       'CreditScoreEeMini', 'NoOfPreviousLoansBeforeLoan',
-       'AmountOfPreviousLoansBeforeLoan', 'TotalIncome', 'TotalLiabilities']] 
-
-
-# In[5]:
-
-
-record
-
-
-# In[ ]:
-
-
-test_case_1 = {
-    'NewCreditCustomer': 0,                # No new credit customer, but no data on previous credit
-    'VerificationType': 'Income_unverified',             # No verification type specified
-    'Age': 0,                              # Age as zero (unrealistic, but tests edge case handling)
-    'Gender': 'Unknown',                   # Gender not specified
-    'AppliedAmount': 0,                     # No loan amount applied for
-    'UseOfLoan': 'None',                    # No loan purpose specified
-    'EmploymentStatus': 'Unknown',          # Employment status unknown
-    'EmploymentDurationCurrentEmployer': 'Not_known', # No employment duration
-    'OccupationArea': 'Not_specified',            # Occupation area not specified
-    'HomeOwnershipType': 'Unknown',        # Homeownership type not specified
-    'TotalIncome': 0,                       # No income reported
-    'TotalLiabilities': 0,                  # No liabilities reported
-    'DebtToIncome': 0,                      # No debt-to-income ratio (effectively zero debt and zero income)
-    'FreeCash': 0,                          # No free cash
-    'Rating': 0,                            # No credit rating
-    'CreditScoreEsMicroL': 'M9',               # No credit score
-    'CreditScoreEeMini': 0,                 # No credit score
-    'NoOfPreviousLoansBeforeLoan': 0,       # No previous loans
-    'AmountOfPreviousLoansBeforeLoan': 0    # No previous loan amounts
-}
-
-
-# In[27]:
 
 
 class LoanProcessor:
@@ -214,16 +136,7 @@ class LoanProcessor:
         # self.insert_to_database(df)
         
         return df
-   
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
